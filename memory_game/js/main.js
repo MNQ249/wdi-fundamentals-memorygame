7be5fa945e4d8cf4a4993528ca22768737286e30
mesfer <mesfer.vip@gmail.com>
2019-09-26 03:32:54 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,21 @@
 alert('Hello, friends.');
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
+
+
 var cards =["queen","queen","king","king"];
 var cardsInPlay =[];
-var cardOne = ["queen"];
-cardsInPlay.push('clyde');
-var cardTwo =["king"];
-cardsInPlay.push("king");
-
-console.log("User flipped " + cardOne);
-console.log(cardsInPlay);
-console.log("User flipped " + cardTwo);
-if(cardsInPlay.length === 2){
-	console.log("The length is " + cardsInPlay.length + " which are [" + cardsInPlay + "]");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
-
-if(cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
 }
-else{
-	alert("Sorry, try again.");
-}
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cardId);
+//console.log(cardsInPlay);
 
-// console.log("User flipped " + cardFour);
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
